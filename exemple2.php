409338fe7e4c7114baec61397ba556745360af38
caioresendede <caioresende14@gmail.com>
2023-11-10 17:52:17 -0300

    segundo php

---
+++ exemple2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $x = 4;
+    $y = 5;
+    echo "x=" . $x; <br>
+    echo "x=" . $x; <br>    
+    echo "x + y= " . $x + $y;
+    
+    ?>
+</body>
+</html>
